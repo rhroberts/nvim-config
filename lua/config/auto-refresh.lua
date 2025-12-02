26b1cfcf8a3f816bc2bcbfe254b13aeb60b53272
Rusty Roberts <rusty@1ap.com>
2025-12-02 09:12:06 -0800

    chore: pkg updates; disable ggutters autocmd

--- lua/config/auto-refresh.lua
+++ lua/config/auto-refresh.lua
@@ -13,13 +13,14 @@ if timer then
 end
 
 -- Refresh gitsigns when focus is regained or files change
-vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
-  callback = function()
-    local gs = package.loaded['gitsigns']
-    if gs then
-      vim.schedule(function()
-        vim.cmd('silent! Gitsigns refresh')
-      end)
-    end
-  end,
-})
+-- NOTE: Disabled - was causing gutters to blink on cursor movement
+-- vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
+--   callback = function()
+--     local gs = package.loaded['gitsigns']
+--     if gs then
+--       vim.schedule(function()
+--         vim.cmd('silent! Gitsigns refresh')
+--       end)
+--     end
+--   end,
+-- })

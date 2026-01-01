01c790ea25ccb27433e8287178e4a9f3115f7b58
Rusty Roberts <rusty@1ap.com>
2026-01-01 12:19:59 -0800

    fix: Lazy styling

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,8 @@ require('lazy').setup({
   { import = 'plugins' },
 }, {
   ui = {
+    border = 'rounded',
+    backdrop = 100,
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {

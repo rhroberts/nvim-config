209b562a0c9fbb89fac32ac3e0694b7768385ea3
Rusty Roberts <rusty@1ap.com>
2026-01-01 10:58:05 -0800

    feat: use fidget for noties

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,8 +24,14 @@ return {
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      {
+        'j-hui/fidget.nvim',
+        opts = {
+          notification = {
+            override_vim_notify = true,
+          },
+        },
+      },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',

78056dabeaec218143593f7b86df487565721916
Rusty Roberts <rusty@1ap.com>
2026-01-30 11:00:12 -0800

    feat: rust lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -167,7 +167,7 @@ return {
         clangd = {},
         -- gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- TypeScript is handled by typescript-tools.nvim (see lua/plugins/typescript-tools.lua)
